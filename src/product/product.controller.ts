96d2eb5829b9790c796c02ba507d946d00348014
ViacheslavDV <viacheslavdev279@gmail.com>
2023-02-20 17:39:25 +0200

    server last version

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,4 @@
-import { ISortByRating, ISortByPrice, ISortByDate } from './types';
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Query } from '@nestjs/common/decorators';
 
@@ -8,32 +7,15 @@ export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get()
-  findAll(@Query('searchTerm') searchTerm?: string) {
-    return this.productService.findAll(searchTerm.toLowerCase());
-  }
-
-  @Get()
-  orderByPrice(@Query('sortByPrice') sortByPrice?: ISortByPrice) {
-    return this.productService.orderByPrice(sortByPrice);
-  }
-
-  @Get()
-  orderByRating(@Query('sortByRating') sortByRating?: ISortByRating) {
-    return this.productService.orderByRating(sortByRating);
-  }
-
-  @Get()
-  orderByDate(@Query('sortByDate') sortByDate?: ISortByDate) {
-    return this.productService.orderByDate(sortByDate);
-  }
-
-  @Get(':id')
-  findOneById(@Param('id') id: string) {
-    return this.productService.findOneById(+id);
-  }
-
-  @Get('similar/:id')
-  findSimilarProducts(@Param('id') id: string) {
-    return this.productService.findSimilarProducts(+id);
+  searchPlusOrder(
+    @Query('searchQuery') searchQuery?: string,
+    @Query('sortByPar') sortByPar?: 'price' | 'rating' | 'createdAt',
+    @Query('orderByPar') orderByPar?: 'asc' | 'desc',
+  ) {
+    return this.productService.searchPlusOrder(
+      searchQuery,
+      sortByPar,
+      orderByPar,
+    );
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,85 +1,27 @@
-import { ISortType, ISortByPrice, ISortByRating, ISortByDate } from './types';
 import { PrismaService } from './../prisma.service';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductService {
   constructor(private prisma: PrismaService) {}
 
-  findOneById(id: number) {
-    const product = this.prisma.product.findUnique({
-      where: {
-        id,
-      },
-    });
-    if (!product) throw new NotFoundException('Product not found');
-    return product;
-  }
-
-  async findAll(searchTerm?: string) {
-    return this.prisma.product.findMany(
-      searchTerm && {
-        where: {
-          OR: [
-            {
-              name: {
-                contains: searchTerm,
-                mode: 'insensitive',
-              },
-            },
-            {
-              description: {
-                contains: searchTerm,
-                mode: 'insensitive',
-              },
-            },
-          ],
-        },
-      },
-    );
-  }
-
-  async orderByPrice(sortByPrice?: ISortByPrice) {
-    return this.prisma.product.findMany(
-      sortByPrice && {
-        orderBy: {
-          // createdAt: sortType === 'oldest' ? 'asc' : 'desc',
-          // price: sortByPrice === 'low-to-high' ? 'asc' : 'desc',
-          price: 'desc',
-        },
-      },
-    );
-  }
-
-  async orderByRating(sortByRating?: ISortByRating) {
-    return this.prisma.product.findMany(
-      sortByRating && {
-        orderBy: {
-          // createdAt: sortType === 'oldest' ? 'asc' : 'desc',
-          rating: sortByRating === 'low-to-high' ? 'asc' : 'desc',
-        },
-      },
-    );
-  }
-
-  async orderByDate(sortByDate?: ISortByDate) {
-    return this.prisma.product.findMany(
-      sortByDate && {
-        orderBy: {
-          // createdAt: sortType === 'oldest' ? 'asc' : 'desc',
-          rating: sortByDate === 'oldest' ? 'asc' : 'desc',
-        },
-      },
-    );
-  }
-
-  findSimilarProducts(currentProductId: number) {
+  async searchPlusOrder(
+    searchQuery?: string,
+    orderByPar?: 'price' | 'rating' | 'createdAt',
+    sortByPar?: 'asc' | 'desc',
+  ) {
     return this.prisma.product.findMany({
       where: {
-        id: {
-          not: currentProductId,
+        name: {
+          contains: searchQuery,
+          mode: 'insensitive',
         },
       },
+      orderBy: [
+        {
+          [orderByPar]: sortByPar,
+        },
+      ],
     });
   }
 }
